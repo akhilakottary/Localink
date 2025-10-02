a28055d0cc454950b6d70385cc785831bd83a44e
akhilakottary <git config --global user.email akhilakottary26@gmail.com>
2025-10-03 01:21:25 +0530

    feat(landing-page): initial implementation of user landing page with header, tagline, and Get Started button

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import LandingPage from './pages/user/landing';
+//import UserDashboard from './pages/user/user-dashboard'; // New import
+//import WorkerDetail from './pages/user/work-details'; // New import
+	
+function App() {
+  return (
+    <Router>
+      <div className="app-container">
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          {/* <Route path="/user-dashboard" element={<UserDashboard />} />
+          <Route path="/worker/:id" element={<WorkerDetail />} /> */}
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
+
